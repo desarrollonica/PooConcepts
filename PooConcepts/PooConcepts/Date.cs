855bc39073f1839f78c77ef19a84ef8dd5238a6d
desarrollonica <desarrollonica@gmail.com>
2024-01-06 00:23:37 -0600

    Finish Class Date

---
+++ PooConcepts/PooConcepts/Date.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PooConcepts
+{
+    public class Date
+    {
+        // Definimos nuestros atributos privados
+        private int _year;
+        private int _month;
+        private int _day;
+
+        // Snipets para contuctor (ctor + TAB + TAB)
+        public Date(int year, int month, int day)
+        {
+            _year = year;
+            _month = CheckMonth(month);
+            _day = CheckDay(day, month, year); 
+        }
+
+        private int CheckDay(int day, int month, int year)
+        {
+            // Validamos Bisiesto (Cada 4 si, cada 100 no, cada 400 si)
+            if (month == 2 && day == 29 && IsLeapYear(year))
+                return day;
+            
+            // Declaramos arreglo de días por mes 
+            int[] daysPerMonth = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+            if (day >= 1 && day <= daysPerMonth[month])
+                return day;
+
+            throw new DayException($"Invalid day: {day}");
+        }
+
+        private bool IsLeapYear(int year)
+        {
+            return year % 400 == 0 || year % 4 == 0 && year % 100 != 0;
+
+        }
+
+        private int CheckMonth(int month)
+        {
+            if (month >= 1 && month <=12)
+                return month;
+
+            throw new MonthException($"Invalid month: {month}");
+        }
+
+        // Sobreescribimos el ToString
+        public override string ToString()
+        {
+            return $"{_day:00}/{_month:00}/{_year}";
+        }
+    }
+}
---
+++ PooConcepts/PooConcepts/Program.cs
@@ -0,0 +1,16 @@
+﻿using PooConcepts;
+
+Console.WriteLine("Conceptos de POO");
+Console.WriteLine("----------------");
+
+try
+{
+	Console.WriteLine(new Date(2024, 01, 05));
+	Console.WriteLine(new Date(2024, 7, 25));
+	Console.WriteLine(new Date(2024, 11, 12));
+    Console.WriteLine(new Date(2024, 02, 29));
+}
+catch (Exception error)
+{
+	Console.WriteLine(error.Message);
+}
